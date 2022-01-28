50688c65635c4c269ae4d055cd30fdf6b8ee0631
xundaoxd <xundaoxd@outlook.com>
2022-01-28 16:21:12 +0800

    simplify entry

--- entry.sh
+++ entry.sh
@@ -6,12 +6,13 @@ docker-app() {
         -h \"$(hostname)\" \
         -v \$PWD:/Home -w /Home \
         -v $HOME/Workdir/docker-apps/.home:/home/xundaoxd \
+        -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix \
         -e LANG=C.UTF-8 -e GTK_IM_MODULE=xim -e XMODIFIERS"
 
     alias node="$DOCKER_PREFIX --network host -ti node:latest node"
+    alias npx="$DOCKER_PREFIX --network host -ti node:latest npx"
     alias npm="$DOCKER_PREFIX --network host -ti node:latest npm"
     alias yarn="$DOCKER_PREFIX --network host -ti node:latest yarn"
-    alias npx="$DOCKER_PREFIX --network host -ti node:latest npx"
 
     alias go="$DOCKER_PREFIX -ti xundaoxd/go:latest go"
 
@@ -22,18 +23,15 @@ docker-app() {
     alias cuda-cmake="$DOCKER_PREFIX -ti --gpus all xundaoxd/cuda-cmake:latest cmake"
 
     alias obsidian="$DOCKER_PREFIX \
-        -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix \
         -v $HOME/Downloads:/home/xundaoxd/Downloads \
         -v $HOME/Pictures:/home/xundaoxd/Pictures \
         -v $HOME/Documents:/home/xundaoxd/Documents \
         -v $HOME/Desktop:/home/xundaoxd/Desktop \
         -d xundaoxd/obsidian:latest"
     alias octave="$DOCKER_PREFIX \
-        -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix \
         -d xundaoxd/octave:latest \
         octave --gui"
     alias vlc="$DOCKER_PREFIX \
-        -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix \
         -e PULSE_SERVER=unix:/run/user/1000/pulse/native \
         -v /run/user/1000/pulse:/run/user/1000/pulse \
         -d xundaoxd/vlc:latest"
