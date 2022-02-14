9fcc1f38817c49e20ced9b69f61e5980855b15b7
xundaoxd <xundaoxd@outlook.com>
2022-02-14 10:04:06 +0800

    update entry.h

--- entry.sh
+++ entry.sh
@@ -9,7 +9,7 @@ base-docker() {
         -v $HOME/Documents:/home/xundaoxd/Documents:ro \
         -v $HOME/Desktop:/home/xundaoxd/Desktop:ro \
         -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix \
-        -e LANG="en_US.UTF-8" -e GTK_IM_MODULE="xim" -e QT_IM_MODULE="xim" -e XMODIFIERS \
+        -e LANG="C.UTF-8" -e GTK_IM_MODULE="xim" -e QT_IM_MODULE="xim" -e XMODIFIERS \
         "$@"
 }
 
